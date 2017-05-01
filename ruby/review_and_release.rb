c68969a4ca9a92a725b1ab10686b0ac3750acad0
Steve Rector <steverector27@gmail.com>
2017-05-01 01:46:56 -0700

    Desired functionality achieved

--- ruby/review_and_release.rb
+++ ruby/review_and_release.rb
@@ -23,13 +23,12 @@ def add_release(collection, game, year)
   collection[game][:year] = year 
 end 
 
-# TEST CODE 
-
 # UI 
 
 # Let user choose a game to insert into collection 
 # Let them add games until 'done' or until there are 5 game 
-count = 1
+
+count = 0
 until count == 5 
   puts "What game will we be adding today?"
   game_addition = gets.chomp
@@ -40,20 +39,31 @@ until count == 5
 end
 
 # Let user add a review and a release year to the games 
-puts "If there is a game you would like to review, state so now (or type 'done')"
-game_name = gets.chomp 
-if game_name == 'done'
-  puts "Thank you!"
-end 
-games_collection.each_key do |game|
-  if game == game_name 
-    puts "Awesome, and what score would you give #{game}?"
-    score = gets.chomp.to_i
-    games_collection[game][:review] = score 
-  end 
-end
+# let them keep adding until length of collection 
 
-puts games_collection
+count = 0 
+until count == games_collection.length
+  puts "If there is a game you would like to review and add a release date for, state so now (or type 'done')"
+  game_name = gets.chomp 
+  break if game_name == 'done'
+  games_collection.each_key do |game|
+    if game == game_name 
+      puts "Awesome, and what score would you give #{game}?"
+      score = gets.chomp.to_i
+      add_review(games_collection, game, score)
+      puts "And the release year?"
+      release = gets.chomp.to_i
+      add_release(games_collection, game, release)
+      puts games_collection
+      count +=1
+    end 
+  end
+end
+puts "Thank you for sharing your game collection with us!"
+puts "Your current collection is...."
+games_collection.each do |game, info|
+  puts "#{game} --- Review is #{info[:review]}, Release was in #{info[:year]}"
+end
 
 
 
